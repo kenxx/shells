98a84ef503056cea33ecb6f136a0ffc1d6d9ac7c
Kenneth Hawk <me@kenx.cn>
2017-11-17 07:20:23 +0000

    resolve file

--- set-git.sh
+++
@@ -1,4 +0,0 @@
-git config --global user.name "Kenneth Hawk"
-git config --global user.email "me@kenx.cn"
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=2678400'
