d408a54af253dd58447cda2a2260d4f482da9931
Kenneth <work@kenx.cn>
2018-04-27 23:22:42 -0400

    add something

--- bash/set-git.sh
+++ bash/set-git.sh
@@ -16,18 +16,26 @@ do
 		echo "Usage: `basename $0` [-u user] [-e email]"
 		;;
 	esac
-	shift
 done
+
+shift $(($OPTIND-1))
 
 if [[ $User ]]
 then
+	echo "Set User($User) to global settings."
 	git config --global user.name "${User}"
 fi
 
 if [[ $Email ]]
 then
+	echo "Set Email($Email) to global settings."
 	git config --global user.email "${Email}"
 fi
 
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=2678400'
+
+
+git config --list --global
+
+#eof
